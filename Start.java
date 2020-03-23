2177210a87150be1443b8451c52aa0c7c58c06c8
Vinstan <vinstanchurchill@gmail.com>
2020-03-22 23:20:52 -0700

    first commint

---
+++ Start.java
@@ -0,0 +1,10 @@
+public class Start
+{
+    public static void main(String args[])
+    {
+        char a = 97, b = 66, c = 67;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c); 
+    }
+}
